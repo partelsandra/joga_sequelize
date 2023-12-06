e4cde31433bab674f4f8cd8c2fffd3bf2b502f0c
partelsandra <sandra.partel.001@voco.ee>
2023-12-06 19:50:01 +0200

    Created connection to database

--- index.js
+++ index.js
@@ -4,10 +4,24 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Connect to the database
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequelize')
+
+// Testing connection
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connected to the database');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database', err); // Add a comma here
+    });
+
 app.get("/", (req, res) => {
-    res.json({message: "Welcome to sequelize application"});
+    res.json({ message: "Welcome to sequelize application" });
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000')
+    console.log('Server is running on http://localhost:3000');
 });
