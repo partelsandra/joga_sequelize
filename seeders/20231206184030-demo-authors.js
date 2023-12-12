7feb12b6eb50389da701e145bb4010a762f9fb64
partelsandra <sandra.partel.001@voco.ee>
2023-12-12 12:16:46 +0200

    Created author table and added table data seeder fixed

--- seeders/20231206184030-demo-authors.js
+++ seeders/20231206184030-demo-authors.js
@@ -1,27 +1,15 @@
 'use strict';
 
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    return Promise.all([
-      queryInterface.bulkInsert('Authors', [{
-        name: 'Ashley Galvin',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }]),
-      queryInterface.bulkInsert('Authors', [{
-        name: 'Patrick Beach',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }]),
-      queryInterface.bulkInsert('Authors', [{
-        name: 'MacKenzie Miller',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }])
-    ]);
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('authors', [
+      { name: 'Ashley Galvin', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Patrick Beach', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'MacKenzie Miller', createdAt: new Date(), updatedAt: new Date() }
+    ], {});
   },
 
-  async down(queryInterface, Sequelize) {
-    return queryInterface.bulkDelete('Authors', null, {});
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('authors', null, {});
   }
 };
