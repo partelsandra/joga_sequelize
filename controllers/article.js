f19ed779559fb0508178e85eac1ad04e37f5bea8
partelsandra <sandra.partel.001@voco.ee>
2023-12-12 13:22:43 +0200

    Created new article

---
+++ controllers/article.js
@@ -0,0 +1,39 @@
+// Get connection to database ORM object
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequelize')
+// Read models data for table representation
+const models = require('../models');
+
+// Create new article into data table
+const createArticle = (req, res) => {
+   // get form data
+    let name = req.body.name
+    let slug = req.body.slug
+    let image = req.body.image
+    let body = req.body.body
+
+    //create new article by Article model
+    const newArticle = models.Article.create({
+        // Add values for NOT NULL fields
+        // Left one: data table fields,
+        // right one: values from form
+        name: name,
+        slug: slug,
+        image: image,
+        body: body,
+        // Publish date generate as now
+        published: new Date().toISOString().slice(0, 19).replace('T', ' ')
+    })
+        .then(article => {
+            console.log(article);
+            return res.status(200).json({ message: 'New article is added' });
+        })
+        .catch(error => {
+            return res.status(500).send(error.message);
+        });
+};
+
+// Export controller functions
+module.exports = {
+    createArticle
+};
--- index.js
+++ index.js
@@ -22,6 +22,11 @@ app.get("/", (req, res) => {
     res.json({ message: "Welcome to sequelize application" });
 });
 
+//using routes and controllers
+const articleRouter = require('./routes/article');
+app.use('/', articleRouter);
+app.use('/article', articleRouter);
+app.use('admin/article', articleRouter)
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
